320a98afbbf3a3f4733d084f370e183e5a2851fd
MalikSaadUllahNasir <21pwdsc0025@uetpeshawar.edu.pk>
2023-12-13 08:28:48 -0800

    inital commit

---
+++ Task_1/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const fs = require('fs').promises;
+const app = express();
+app.use(express.json());
+
+const jsonFileName = 'data.json'; // JSON file name
+
+// Create initial JSON content in the file (for demonstration)
+const initialJSONContent = { message: 'Initial content' };
+fs.writeFile(jsonFileName, JSON.stringify(initialJSONContent, null, 2), (err) => {
+    if (err) throw err;
+    console.log('JSON file created with initial content!');
+});
+
+// GET /readFile Endpoint
+app.get('/readFile', async (req, res) => {
+    try {
+        const jsonData = await fs.readFile(jsonFileName, 'utf8');
+        res.send(JSON.parse(jsonData));
+    } catch (error) {
+        res.status(404).send('File not found.');
+    }
+});
+
+// POST /writeFile Endpoint
+app.post('/writeFile', async (req, res) => {
+    const { data } = req.body;
+    try {
+        if (!data) throw new Error('No data provided.');
+        await fs.writeFile(jsonFileName, JSON.stringify(data, null, 2));
+        res.send('File written successfully.');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+// PUT /updateFile Endpoint
+app.put('/updateFile', async (req, res) => {
+    const { newData } = req.body;
+    try {
+        if (!newData) throw new Error('No new data provided.');
+
+        // Read the existing JSON content
+        const jsonData = await fs.readFile(jsonFileName, 'utf8');
+        const existingData = JSON.parse(jsonData);
+
+        // Append newData to existingData
+        existingData.message += `\n${newData}`;
+
+        // Write updated JSON content back to the file
+        await fs.writeFile(jsonFileName, JSON.stringify(existingData, null, 2));
+        res.send('File updated successfully.');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost: ${PORT}`);
+});
